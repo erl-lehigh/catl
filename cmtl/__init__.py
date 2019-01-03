b2b770bc2fd56d5ee50e7d03b2508578e7d7e133
Cristian-Ioan Vasile <cvasile@mit.edu>
2019-01-02 19:28:33 -0500

    Made cmtl a module and exposed API.

---
+++ cmtl/__init__.py
@@ -0,0 +1,8 @@
+'''
+ Copyright (C) 2018 Cristian Ioan Vasile <cvasile@bu.edu>
+ Hybrid and Networked Systems (HyNeSs) Group, BU Robotics Lab, Boston University
+ See license.txt file for license information.
+'''
+
+from cmtl import Operation, CMTLFormula, CapabilityRequest
+from cmtl2stl import cmtl2stl
