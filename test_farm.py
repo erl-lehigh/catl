fab2ff5962c2f550bb53a3fafd1ef79209f605e3
Cristian-Ioan Vasile <cvasile@lehigh.edu>
2020-08-11 13:08:25 -0400

    Set up logging in farm test.

--- test_farm.py
+++ test_farm.py
@@ -6,15 +6,33 @@
  See license.txt file for license information.
 '''
 
+import sys
+import logging
+
 from lomap import Ts
 
 from route_planning import route_planning
 from visualization import show_environment
 
 
+def setup_logging(logfile='test_farm.log', loglevel=logging.DEBUG):
+    fs, dfs = '%(asctime)s %(levelname)s %(message)s', '%m/%d/%Y %I:%M:%S %p'
+
+    if logfile is not None:
+        logging.basicConfig(filename=logfile, level=loglevel,
+                            format=fs, datefmt=dfs)
+
+    root = logging.getLogger()
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(loglevel)
+    ch.setFormatter(logging.Formatter(fs, dfs))
+    root.addHandler(ch)
+
+
 def case_farm(ts_filename='farm.yaml'):
     '''TODO:
     '''
+    setup_logging()
 
     ts = Ts.load(ts_filename)
     for u, v in ts.g.edges():
@@ -36,13 +54,13 @@ def case_farm(ts_filename='farm.yaml'):
     for state, _ in agents:
         assert state in ts.g, 'State "{}" not in TS!'.format(state)
 
-    specification = 'TODO:'
-    # Example
-    'F[0, 2] T(4, test, {(a, 2), (b, 3)})'
-    '&& G[1, 7] T(2, test, {(a, 1), (c, 4)})'
-    '&& F[3, 5] T(3, test2, {(b, 1), (d, 2)})'
+    specification = ('F[0, 2] T(4, blue, {(UV, 2), (Mo, 1)})'
+                     # '&& G[1, 4] T(2, green, {(IR, 1), (Vis, 3)})'
+                     # '&& F[3, 4] T(3, yellow, {(IR, 3), (Vis, 2), (UV, 3), (Mo, 4)})'
+                     )
 
-#     route_planning(ts, agents, specification)
+    m = route_planning(ts, agents, specification)
+
 
 if __name__ == '__main__':
     case_farm()
